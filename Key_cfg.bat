24943d6fdfed8b721ed35b31242f79008b132431
SpellCaster <spellcaster@bk.ru>
2012-12-17 18:20:51 +0400

    Init

---
+++ Key_cfg.bat
@@ -0,0 +1,10 @@
+:: This is config file for Sign.bat. It contains certificate & key properties.
+:: Don't try to launch it!
+:: If you don't have your keystore yet, just modify these values as you like (or leave unchanged)
+:: and the script will generate all the needed stuff.
+
+:: Modify this value to yours!
+set StorePass=yourpass
+set KeyPass=yourpass
+set Alias=youralias
+set KeyStore=%CDir%\key.keystore
